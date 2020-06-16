3cf13b9ed129dbdbaeed7c73ff7db92115d0b1bd
Vyshnav <vyshnav@devopsenabler.com>
2020-06-16 06:11:10 +0000

    updated

--- terraformeksscript/providers.tf
+++ terraformeksscript/providers.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIAXU5FK22URYWKN2I7"
-  secret_key = "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"
+  access_key = "     "
+  secret_key = "     "
 }
 
 data "aws_region" "current" {
